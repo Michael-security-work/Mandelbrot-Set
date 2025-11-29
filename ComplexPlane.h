761c0c5de5d6cdd1823c19f1782bdaa772d2e95f
Ikinonga <mihail2006.27.mn@gmail.com>
2025-11-28 19:23:28 -0800

    f

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -29,6 +29,8 @@ class ComplexPlane : public Drawable
 		
 		size_t countIterations(Vector2f coord);
 		void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b); //now what? 2
+		//
+		//
 		Vector2f mapPixelToCoords(Vector2i mousePixel);
 		
 	public:
